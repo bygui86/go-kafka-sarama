4f3fc82751393ac6f51787aa48624f898b3854b7
Matteo Baiguini <baiguini.matteo@gmail.com>
2019-03-26 15:06:51 +0100

    Added producer

---
+++ producer/producer.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/Shopify/sarama"
+)
+
+const (
+	kafkaConn = "dockerhost:9092"
+	topic     = "gomessages"
+)
+
+func main() {
+	// create producer
+	producer, err := initProducer()
+	if err != nil {
+		fmt.Println("Error producer: ", err.Error())
+		os.Exit(1)
+	}
+
+	// read command line input
+	// reader := bufio.NewReader(os.Stdin)
+	var msg string = "testing go-kafka integration "
+	var counter int = 1
+	for {
+		// fmt.Print("Enter msg: ")
+		// msg, _ := reader.ReadString('\n')
+
+		// publish without goroutene
+		publish(msg+strconv.Itoa(counter), producer)
+
+		// publish with go routene
+		// go publish(msg, producer)
+		counter++
+
+		time.Sleep(3 * time.Second)
+	}
+}
+
+func initProducer() (sarama.SyncProducer, error) {
+	// setup sarama log to stdout
+	sarama.Logger = log.New(os.Stdout, "", log.Ltime)
+
+	// producer config
+	config := sarama.NewConfig()
+	config.Producer.Retry.Max = 5
+	config.Producer.RequiredAcks = sarama.WaitForAll
+	config.Producer.Return.Successes = true
+
+	// async producer
+	//prd, err := sarama.NewAsyncProducer([]string{kafkaConn}, config)
+
+	// sync producer
+	prd, err := sarama.NewSyncProducer([]string{kafkaConn}, config)
+
+	return prd, err
+}
+
+func publish(message string, producer sarama.SyncProducer) {
+	// publish sync
+	msg := &sarama.ProducerMessage{
+		Topic: topic,
+		Value: sarama.StringEncoder(message),
+	}
+	p, o, err := producer.SendMessage(msg)
+	if err != nil {
+		fmt.Println("Error publish: ", err.Error())
+	}
+
+	// publish async
+	//producer.Input() <- &sarama.ProducerMessage{
+
+	fmt.Println("Partition: ", p)
+	fmt.Println("Offset: ", o)
+}
